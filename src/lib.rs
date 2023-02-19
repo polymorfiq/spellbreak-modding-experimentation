227f749678ead11c682ef8a45a96fad087e0e8c2
Cory Finger <cory.j.finger@gmail.com>
2023-02-19 14:47:34 -0800

    Clean up code and get the correct (dynamically allocated) base address on initialization

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,10 @@
 #![feature(naked_functions)]
 #![feature(asm_const)]
 use std::arch::asm;
-use std::sync::atomic::{AtomicUsize, Ordering};
 use retour::static_detour;
+use winapi::um::libloaderapi::{GetModuleHandleA, GetProcAddress};
 
-static bp_count: AtomicUsize = AtomicUsize::new(0);
-
-static IDA_BASE_ADDR: usize = 0x7FF627040000;
-static BASE_ADDR: usize = 0x7FF6B6A60000;
+// IDA base address: 0x7FF627040000
 static NEXT_BOT_NAME_FN_OFFSET: usize = 0x9EDB70;
 
 type NextBotNameFn = extern "C" fn(usize, i32);
@@ -17,12 +14,15 @@ static_detour! {
 
 #[ctor::ctor]
 fn ctor() {
+    let base_addr = unsafe { GetModuleHandleA(std::ptr::null()) };
+    println!("INJECTING AT BASE ADDRESS: {:?}", base_addr);
+
     // Output debug message in Server's console
     println!("Succesfully injected spellbreak_mod into g3Server... This is in server's memory space.");
 
     // Detour function call BotManager::set_NextBotNameIdx(self, value) so that it points at our own function
     unsafe {
-        let next_bot_fn_addr = (BASE_ADDR + NEXT_BOT_NAME_FN_OFFSET) as *const (usize, u32);
+        let next_bot_fn_addr = ((base_addr as usize) + NEXT_BOT_NAME_FN_OFFSET) as *const (usize, u32);
         let fn_ptr: NextBotNameFn = std::mem::transmute(next_bot_fn_addr);
         NextBotName.initialize(fn_ptr, nextBotNameShiv).unwrap();
         NextBotName.enable();
@@ -30,11 +30,6 @@ fn ctor() {
 }
 
 fn nextBotNameShiv(me: usize, value: i32) {
-    // This occurs directly after incrementing the index. Let's decrement it
-    unsafe {
-        asm!("sub edx, 1");
-    }
-
-    // Let's do something more complicated and print a message from the server
+    // Let's do this instead of incrementing the counter
     println!("Overrode the thing!");
 }
